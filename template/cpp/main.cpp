fc43bca65170d7b2fbc8df98b9dd4f58f404c183
zhu nianyang <zny2008@gmail.com>
2010-02-03 00:00:00 +0000

    Version 1.0: Initial upload

---
+++ template/cpp/main.cpp
@@ -0,0 +1,18 @@
+/*=============================================================================
+#  Author:          
+#  Email:           
+#  FileName:        
+#  Version:         
+#  LastChange:      
+#  Description:     
+#  History:         
+=============================================================================*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+int main(int argc,char* argv[])
+{
+	return 0;
+}
